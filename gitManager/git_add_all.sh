e7d78c75a277b4135f2f95df7dc670a45e92cc93
shosalli <shosalli220599@gmail.com>
2022-04-10 18:32:03 +0530

    regular update

---
+++ gitManager/git_add_all.sh
@@ -0,0 +1,4 @@
+# git add ../GitHub_manager/
+# git add ../goDeep/
+# git add ../.
+git add .
---
+++ zRunAllGit.sh
@@ -0,0 +1,5 @@
+# sh /GitHub_manager/git_add_all.sh
+# clear
+. gitManager/git_add_all.sh
+. gitManager/git_commit.sh
+. gitManager/git_push.sh
